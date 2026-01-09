fd1ae708cd1086103ac6d650b8c84df68764f23a
mtnvencenzo <rvecchi@gmail.com>
2026-01-08 17:43:29 -0700

    feat (oauth): Replacing oauth code with pypi package

--- embedding-agent/src/cocktails_embedding_agent/app_module.py
+++ embedding-agent/src/cocktails_embedding_agent/app_module.py
@@ -1,4 +1,8 @@
 from cezzis_kafka import KafkaConsumerSettings
+from cezzis_oauth import (
+    IOAuthTokenProvider,
+    OAuthTokenProvider,
+)
 from injector import Binder, Injector, Module, noscope, singleton
 from mediatr import Mediator
 
@@ -18,10 +22,6 @@ from cocktails_embedding_agent.domain.config import (
 from cocktails_embedding_agent.domain.config.oauth_options import OAuthOptions
 from cocktails_embedding_agent.infrastructure.clients.aisearch_api.aisearch_client import AISearchClient
 from cocktails_embedding_agent.infrastructure.clients.aisearch_api.iaisearch_client import IAISearchClient
-from cocktails_embedding_agent.infrastructure.clients.oauth.oauth_token_provider import (
-    IOAuthTokenProvider,
-    OAuthTokenProvider,
-)
 from cocktails_embedding_agent.infrastructure.eventing.embedding_event_receiver import EmbeddingEventReceiver
 
 
@@ -35,13 +35,22 @@ def mediator_manager(handler_class, is_behavior=False):
 
 class AppModule(Module):
     def configure(self, binder: Binder):
+        oauth_options = get_oauth_options()
+        oauth_token_provider = OAuthTokenProvider(
+            domain=oauth_options.domain,
+            client_id=oauth_options.client_id,
+            client_secret=oauth_options.client_secret,
+            audience=oauth_options.audience,
+            scope=oauth_options.scope,
+        )
+
         binder.bind(Mediator, Mediator(handler_class_manager=mediator_manager), scope=singleton)
         binder.bind(kafka_options.KafkaOptions, get_kafka_options(), scope=singleton)
         binder.bind(KafkaConsumerSettings, get_kafka_consumer_settings(), scope=singleton)
         binder.bind(AppOptions, get_app_options(), scope=singleton)
         binder.bind(AISearchApiOptions, get_aisearch_api_options(), scope=singleton)
-        binder.bind(OAuthOptions, get_oauth_options(), scope=singleton)
-        binder.bind(IOAuthTokenProvider, OAuthTokenProvider, scope=singleton)
+        binder.bind(OAuthOptions, oauth_options, scope=singleton)
+        binder.bind(IOAuthTokenProvider, oauth_token_provider, scope=singleton)
         binder.bind(RunEmbeddingAgentCommandHandler, RunEmbeddingAgentCommandHandler, scope=singleton)
         binder.bind(IAISearchClient, AISearchClient, scope=singleton)
         binder.bind(EmbeddingEventReceiver, EmbeddingEventReceiver, scope=noscope)
