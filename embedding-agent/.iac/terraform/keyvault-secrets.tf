f8e4b1b0cadac9e6e8d988f78e40e98848e13d42
mtnvencenzo <rvecchi@gmail.com>
2026-03-11 21:29:11 -0600

    fix(tf): resource name for secret

--- embedding-agent/.iac/terraform/keyvault-secrets.tf
+++ embedding-agent/.iac/terraform/keyvault-secrets.tf
@@ -1,4 +1,4 @@
-resource "azurerm_key_vault_secret" "cocktails_aisearch_tei_embeddings_apikey" {
+resource "azurerm_key_vault_secret" "cocktails_embedding_agent_oauth_client_secret" {
   name         = "cocktails-embedding-agent-oauth-client-secret"
   value        = "n/a"
   key_vault_id = data.azurerm_key_vault.cocktails_keyvault.id
