90064335927781f7a459e295c54d627a34cc1e61
mtnvencenzo <rvecchi@gmail.com>
2026-04-08 20:14:59 -0600

    feat(kv): adding onprem keyvault form embedding agent

--- embedding-agent/.iac/terraform/datasources.tf
+++ embedding-agent/.iac/terraform/datasources.tf
@@ -3,7 +3,7 @@ data "azurerm_resource_group" "cocktails_resource_group" {
   name = "rg-${var.sub}-${var.region}-${var.environment}-${var.domain}-${var.sequence}"
 }
 
-data "azurerm_key_vault" "cocktails_keyvault" {
-  name                = "kv-${var.sub}-${var.region}-${var.environment}-${var.shortdomain}-${var.short_sequence}"
+data "azurerm_key_vault" "cocktails_onprem_keyvault" {
+  name                = "kv-${var.sub}-${var.region}-prm-${var.shortdomain}-${var.short_sequence}"
   resource_group_name = data.azurerm_resource_group.cocktails_resource_group.name
 }
--- embedding-agent/.iac/terraform/keyvault-secrets.tf
+++ embedding-agent/.iac/terraform/keyvault-secrets.tf
@@ -1,7 +1,7 @@
 resource "azurerm_key_vault_secret" "cocktails_embedding_agent_oauth_client_secret" {
   name         = "cocktails-embedding-agent-oauth-client-secret"
   value        = "n/a"
-  key_vault_id = data.azurerm_key_vault.cocktails_keyvault.id
+  key_vault_id = data.azurerm_key_vault.cocktails_onprem_keyvault.id
 
   lifecycle {
     ignore_changes = [value]
